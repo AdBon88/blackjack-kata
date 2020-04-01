5bf6f349f7d6e15de1dc5ffff41d104f808f6a46
Adam Bonarrigo <adam.bonarrigo@myob.com>
2020-04-01 17:20:31 +1100

    Duplicate card in deck test now passes, can now correctly calculate the value of a hand of n cards even if hand contains aces or face cards

--- blackjack-kata-test/HandTests.cs
+++ blackjack-kata-test/HandTests.cs
@@ -6,17 +6,52 @@ namespace blackjack_kata
     public class HandTests
     {
         [Fact]
-        public void Hand_HandValueEqualsSumOfCardValues()
+        public void Hand_HandValueIsSumOfCardValues_NoAcesOrFaceCards()
         {
         //Given
-        List<Card> cards = new List<Card>{new Card(CardRank.THREE, CardSuit.CLUB), new Card(CardRank.KING, CardSuit.DIAMOND)};
+        List<Card> cards = new List<Card>{new Card(CardRank.THREE, CardSuit.CLUB), new Card(CardRank.FIVE, CardSuit.DIAMOND)};
         Hand hand = new Hand(cards);
 
         //When
-        const int expected = 13;
-        int actual = hand.Sum;
+        const int expected = 8;
+        int actual = hand.CalculateValue();
         //Then
         Assert.Equal(expected, actual);
         }
+
+        [Theory]
+        [InlineData(CardRank.KING, CardSuit.DIAMOND, CardRank.TEN, CardSuit.HEART, 20)]
+        [InlineData(CardRank.QUEEN, CardSuit.DIAMOND, CardRank.FIVE, CardSuit.CLUB, 15)]
+        [InlineData(CardRank.JACK, CardSuit.SPADE, CardRank.QUEEN, CardSuit.SPADE, 20)]
+        public void Hand_HandValueIsSumOfCardValues_HasFaceCardsNoAces(CardRank card1Rank, CardSuit card1Suit, CardRank card2Rank, CardSuit card2Suit, int expected)
+        {
+        //Given
+        List<Card> cards = new List<Card>{new Card(card1Rank, card1Suit), new Card(card2Rank, card2Suit)};
+        Hand hand = new Hand(cards);
+
+        //When
+        int actual = hand.CalculateValue();
+        //Then
+        Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData(CardRank.ACE, CardSuit.DIAMOND, CardRank.TEN, CardSuit.HEART, CardRank.FIVE, CardSuit.SPADE, 16)]
+        [InlineData(CardRank.ACE, CardSuit.DIAMOND, CardRank.FOUR, CardSuit.CLUB, CardRank.SIX, CardSuit.HEART, 21)]
+
+        public void Hand_HandValueIsSumOfCardValues_HasFaceCardsAndAces(CardRank card1Rank, CardSuit card1Suit, CardRank card2Rank, 
+            CardSuit card2Suit, CardRank card3Rank, CardSuit card3Suit, int expected)
+        {
+        //Given
+        List<Card> cards = new List<Card>{new Card(card1Rank, card1Suit), new Card(card2Rank, card2Suit), new Card(card3Rank, card3Suit)};
+        Hand hand = new Hand(cards);
+
+        //When
+        int actual = hand.CalculateValue();
+        //Then
+        Assert.Equal(expected, actual);
+        }
+
+
     }
 }
--- blackjack-kata/Hand.cs
+++ blackjack-kata/Hand.cs
@@ -1,3 +1,4 @@
+using System.Reflection.Metadata;
 using System;
 using System.Collections.Generic;
 
@@ -6,27 +7,44 @@ namespace blackjack_kata
     public class Hand
     {
         public List<Card> Cards {get;} = new List<Card>();
-        
+        private int _value = 0;
 
         public Hand(List<Card> newHand)
         {
             Cards = newHand;
+            _value += CalculateValue();
         }
         
-        public int Sum
+        public int CalculateValue()
         {
-            get
+            int sum = 0;
+            List<Card> aces = new List<Card>();
+
+            foreach (Card card in Cards)
             {
-                int sum = 0;
-                foreach (Card card in Cards)
+                switch(card.Rank)
                 {
-                    sum += (int)card.Rank;
+                    case CardRank.ACE:
+                        aces.Add(card);
+                        break;
+                    case CardRank.JACK:
+                    case CardRank.QUEEN:
+                    case CardRank.KING:
+                        sum += 10;
+                        break;
+                    default:
+                        sum += (int)card.Rank;
+                        break;
                 }
-                return sum;
             }
+            foreach (Card ace in aces)
+            {
+                if(sum > 10)
+                    sum++;
+                else
+                    sum += 11;
+            }
+            return sum;
         }
-
     }
-
-    
 }
